638074eb306af6c54d5c8942d225116961d43f74
20Ns <230090446@aston.ac.uk>
2025-01-16 21:03:03 +0000

    adding a older project but unfinished one

---
+++ movierec/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MovieRecommendation from './MovieRecommendation';
+
+function App() {
+    return (
+        <div className="App">
+            <MovieRecommendation />
+        </div>
+    );
+}
+
+export default App;
