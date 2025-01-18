82249c42faa05e87a0cd059abff84454e81644f9
20ns <230090446@aston.ac.uk>
2025-01-18 01:15:51 +0000

    added the initial files

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav style={{ padding: '1rem', backgroundColor: '#333', color: '#fff' }}>
+            <h2>Movie Recommendation App</h2>
+        </nav>
+    );
+};
+
+export default Navbar;
