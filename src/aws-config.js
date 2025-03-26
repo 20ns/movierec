6685c8325ecdc4cf5d1bdf0ce6f68922c75f192a
Navpreet <230090446@aston.ac.uk>
2025-03-26 19:17:49 +0000

    fixed the issue

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,13 +1,13 @@
-// src/aws-config.js
+// aws-config.js
 const awsConfig = {
   Auth: {
     region: 'eu-north-1',
     userPoolId: 'eu-north-1_x2FwI0mFK',
     userPoolWebClientId: '3ob6cukt0hlea5bef9l233rv5k',
-    clientSecret: '52t2i8jqmnc7dij1ecghsu9niv6tpkskkkitqs3ffqqbir456ao', // Add this
-    authenticationFlowType: 'USER_SRP_AUTH',
+    clientSecret: '52t2i8jqmnc7dij1ecghsu9niv6tpkskkkitqs3ffqqbir456ao',
+    authenticationFlowType: 'USER_PASSWORD_AUTH', // Changed from USER_SRP_AUTH
     oauth: {
-      domain: 'https://account.d1akezqpdr5wgr.amplifyapp.com/', // Fix this domain
+      domain: 'movierec.auth.eu-north-1.amazoncognito.com', // Actual Cognito domain
       scope: ['email', 'openid', 'profile'],
       redirectSignIn: 'https://account.d1akezqpdr5wgr.amplifyapp.com',
       redirectSignOut: 'https://account.d1akezqpdr5wgr.amplifyapp.com',
